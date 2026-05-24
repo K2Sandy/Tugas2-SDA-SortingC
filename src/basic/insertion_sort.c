8d9a4cd4f96c899d002a70df37f56633a6c425d3
K2Sandy <m.kakasandy@gmail.com>
2026-05-24 14:24:57 +0700

    struktur insertion_sort

---
+++ src/basic/insertion_sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// fungsi tampiilin nilai
+void printArray(int arr[], int n, int limit) {
+for (int i = 0; i < limit && i< n; i++) {
+    print("%d", arr[i]);
+}
+    printf("... (menampilkan %d data pertama)\n", limit);
+}
+
+void shuffleData(){
+}
+
+void insertionSort(){
+    
+}
