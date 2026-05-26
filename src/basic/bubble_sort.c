22244687bb01d0b1d80fb431d52064c282be4126
Arinal17 <arinalhaq170107@gmail.com>
2026-05-26 12:59:41 +0700

    Update bubble_sort.c

--- src/basic/bubble_sort.c
+++ src/basic/bubble_sort.c
@@ -4,6 +4,9 @@
 #include "../include/utils.h"
 #include "../include/sorting.h"
 
+#define DATA_SIZE_INT 1000
+#define DISPLAY_LIMIT 10
+
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
