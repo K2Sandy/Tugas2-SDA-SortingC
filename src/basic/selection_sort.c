bd4cbf8a39c7165437293406fff3b5efc75c3f2c
HanifMaulana31 <hanif457@mhs.usk.ac.id>
2026-05-26 13:16:44 +0700

    penambahan fungsi selection sort

---
+++ src/basic/selection_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../../include/sorting.h"
+#include "../../include/utils.h" // Panggil utils untuk fungsi print dan shuffle
+
+void SelectionSort(int arr[], int n){
+    int i, min_index, j;
+    for (i = 0; i < n - 1; i++) {
+        min_index = i;
+        for (j = i + 1; j < n; j++){
+            if ( arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
+        if ( min_index != i){
+            swap_int(&arr[i], &arr[min_index]);
+        }
+    }
+}
