45e7b0697dd95c0f6e2ed0b0c49821ea395ef0b5
Arinal17 <arinalhaq170107@gmail.com>
2026-05-26 12:47:30 +0700

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/utils.h"
 
 
@@ -22,6 +23,28 @@ void shuffleData(int arr[], int n){
 // Fungsi untuk menukar dua integer
 void swap_int(int *a, int *b) { //fungsi swap untuk tukar posisi elemen index
     int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArrayString(char *arr[], int n, int limit) {
+    for (int i = 0; i < limit && i < n; i++) {
+        printf("%d. %s\n", i + 1, arr[i]);
+    }
+    printf("... (menampilkan %d data pertama)\n", limit);
+}
+
+void shuffleDataString(char *arr[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        char *temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+void swap_string(char **a, char **b) {
+    char *temp = *a;
     *a = *b;
     *b = temp;
 }
