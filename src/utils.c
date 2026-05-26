d20ce36a70e58e3c889c436f2191f31ba3801840
Arinal17 <arinalhaq170107@gmail.com>
2026-05-26 12:55:16 +0700

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -48,3 +48,36 @@ void swap_string(char **a, char **b) {
     *a = *b;
     *b = temp;
 }
+
+// Fungsi otomatis membaca dataset kata dari file .txt
+char** read_words_file(const char *filename, int *word_count) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+
+    int capacity = 200000;
+    char **word_list = malloc(capacity * sizeof(char *));
+    *word_count = 0;
+    char buffer[50];
+
+    while (fscanf(file, "%s", buffer) != EOF) {
+        if (*word_count >= capacity) {
+            capacity *= 2;
+            word_list = realloc(word_list, capacity * sizeof(char *));
+        }
+        word_list[*word_count] = malloc((strlen(buffer) + 1) * sizeof(char));
+        strcpy(word_list[*word_count], buffer);
+        (*word_count)++;
+    }
+    fclose(file);
+    return word_list;
+}
+
+// Fungsi membersihkan memori RAM
+void free_word_list(char **word_list, int word_count) {
+    for (int i = 0; i < word_count; i++) {
+        free(word_list[i]);
+    }
+    free(word_list);
+}
