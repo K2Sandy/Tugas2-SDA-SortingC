1931e7a488f759efdad15b58eb20ee54c9824464
K2Sandy <m.kakasandy@gmail.com>
2026-05-26 13:15:03 +0700

    merge_sort add

--- Include/sorting.h
+++ Include/sorting.h
@@ -3,13 +3,13 @@
 
 // Basic Sorting 
 void bubble_sort(int arr[], int n);
-
+    void jalankan_bubble_sort();
 void insertionSort(int arr[], int n);
     void jalankanInsertionSort(); 
 
 // Advance Sorting 
 
-
+void jalankan_merge_sort();
 void quick_sort(char *arr[], int low, int high);
 
 #endif
