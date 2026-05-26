6c6997b037f64e09dad78fe2460e913b26479f12
Hanif Maulana <hanif457@mhs.usk.ac.id>
2026-05-26 11:39:00 +0700

    tambah fungsi swap sebagai fungsi header di utils untuk bisa dipakaki bersama

--- src/utils.c
+++ src/utils.c
@@ -18,4 +18,11 @@ void shuffleData(int arr[], int n){
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+// Fungsi untuk menukar dua integer
+void swap_int(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 }
