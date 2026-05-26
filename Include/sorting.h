73d77363de0143a80266d5f33e99135fca8329e1
Arinal17 <arinalhaq170107@gmail.com>
2026-05-26 12:50:02 +0700

    Update sorting.h

--- Include/sorting.h
+++ Include/sorting.h
@@ -2,10 +2,14 @@
 #define SORTING_H
 
 // Basic Sorting 
+void bubble_sort(int arr[], int n);
+
 void insertionSort(int arr[], int n);
     void jalankanInsertionSort(); 
 
-
 // Advance Sorting 
+
+
+void quick_sort(char *arr[], int low, int high);
 
 #endif
